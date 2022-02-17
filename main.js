49e69cc79cbb1316f072ee0f32d1f1cb356e9d7d
katy millard <katy.millard@gmail.com>
2022-02-17 14:33:04 -0600

    Ask user what they want to buy

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
+import input from 'input'
+
 class ItemNotFound extends Error {
 }
 
 class NoFunds extends Error {
 }
 
-class BankAccount {
-    constructor(starting) {
-        this.balance = starting;
-    }
-}
 class VendingMachine {
     constructor() {
         this.items = new Map();
@@ -17,27 +14,28 @@ class VendingMachine {
         this.items.set('Sock', 5.00);
         this.items.set('Dog', 20.00);
     }
-    buy(account, item) {
+    buy(budget, item) {
         const price = this.items.get(item);
         if (price == undefined) {
             throw new ItemNotFound('The vending machine does not have that item.');
-        } if (price <= account.balance) {
-            account.balance -= price;
+        } if (price <= budget) {
+            budget -= price;
 
             console.log(`You purchased a ${item}.`);
         } else {
-            throw new NoFunds('Not enough money!');
+            throw new NoFunds('Not enough money in the budget!');
         }
     }
 }
 
 class App {
-    static main() {
-        const account = new BankAccount(15);
+    static async main() {
         const machine = new VendingMachine();
-
+        const budget = await input.text('How much money is in your account?');
+        const item = await input.text('What would you like to buy?');
+        
         try {
-            machine.buy(account, 'Lipstick');           
+            machine.buy(budget, item);           
         } catch (error) {
             if (error instanceof ItemNotFound) {
                 console.log('The vending machine does not have that item.');
